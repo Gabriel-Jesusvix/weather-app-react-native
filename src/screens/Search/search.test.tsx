f3e1ad3b08434c7fdde3c7bb84af5536557355c5
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-12-06 14:50:06 -0300

    Feat: Testing integrations

---
+++ src/screens/Search/search.test.tsx
@@ -0,0 +1,19 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/api/mockCityAPIResponse"
+import { fireEvent, render, screen, waitFor } from "@__tests__/utils/CustomRender"
+import { Search } from "@screens/Search"
+import { api } from "@services/api"
+
+describe('Screen: Search', () => {
+  it('should be show city option', async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
+
+    render(<Search />)
+
+    const searchInput = screen.getByTestId('search-input')
+    fireEvent.changeText(searchInput, "Espirito Santo")
+
+    const option = await waitFor(() => screen.findByText(/Espirito Santo/i))
+
+    expect(option).toBeTruthy()
+  })
+})
